6b2745dc2123921ee279dc7dcde7942654992241
russellzparadox <russellzparadox@gmail.com>
2024-12-02 23:15:26 +0330

    added json for parsing json testing to pars the json

---
+++ client.py
@@ -0,0 +1,32 @@
+#
+#   Hello World client in Python
+#   Connects REQ socket to tcp://localhost:5555
+#   Sends "Hello" to server, expects "World" back
+#
+
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(100):
+    message = """{
+"command_type": "os",
+"command_name": "ping",
+"parameters": [
+"127.0.0.1",
+"-n",
+"6"
+]
+}"""
+    print("Sending request %s …" % request)
+    socket.send_json(message)
+
+    #  Get the reply.
+    message = socket.recv()
+    print("Received reply %s [ %s ]" % (request, message))
---
+++ server.py
@@ -0,0 +1,30 @@
+#
+#   Hello World server in Python
+#   Binds REP socket to tcp://*:5555
+#   Expects b"Hello" from client, replies with b"World"
+#
+
+import time
+import zmq
+import json
+import os
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    #  Wait for next request from client
+    message = json.loads(socket.recv_json())
+    print(f"Received request: {message}")
+    if message['command_type'] == 'os':
+        print(message['command_name']+' '+' '.join(message['parameters']))
+        # os.system(message['command']+' '.join(message['parameter']))
+    if message['command_type'] == 'compute':
+        response = eval(message['expression'])
+        socket.send_json(response)
+
+    #  Do some 'work'
+    time.sleep(1)
+
+    #  Send reply back to client
+    socket.send(b"World")
